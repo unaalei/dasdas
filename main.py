34e431c8f9aef723a2704de43c034952f6999f44
unaalei <trejoferreu@gmail.com>
2023-04-15 03:55:35 +0200

    hola

--- main.py
+++ main.py
@@ -60,6 +60,7 @@ def check_for_updates() -> None:
     """
     if (latest_version := get_latest_version()) != MY_VERSION:
      match repeat_prompt_until_valid_input(f"""{Fore.LIGHTGREEN_EX}New Version Available
+                                           
 
 {Fore.LIGHTWHITE_EX}https://github.com/the-cult-of-integral/discord-raidkit/releases/tag/{latest_version}
 
